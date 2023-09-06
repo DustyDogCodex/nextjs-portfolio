aaca6dfecdbe094a1df1e123cd940e445c58200f
vmalaviya <varunmalaviya7@gmail.com>
2023-09-06 13:48:40 -0400

    trying to make animated text effect

--- src/app/components/Hero.tsx
+++ src/app/components/Hero.tsx
@@ -1,20 +1,85 @@
+'use client'
+import { gsap } from "gsap"
+import SplitType from 'split-type'
+
 import Image from "next/image"
+import { useEffect } from "react"
 
 function Hero() {
+    useEffect(() => {
+        const animateText = () => {
+            const subtitles = gsap.utils.toArray('p')
+
+            //gsap timeline for animations
+            const subtitleTimeline = gsap.timeline()
+
+            subtitles.forEach(sub => {
+                const mySub = new SplitType(sub)
+               
+                    subtitleTimeline
+                    .from(mySub.chars, { 
+                        opacity: 0,
+                        y: 80,
+                        rotateX: -90,
+                        stagger: 0.2
+                    }, "<")
+                    .to(mySub.chars, { 
+                        opacity: 1,
+                        y: -80,
+                        rotateX: 90,
+                        stagger: 0.2
+                    }, "<1")
+                
+            })
+        }
+        animateText()
+    }, [])
+
     return (
         <section
-            className="container grid grid-cols-1 lg:grid-cols-12 border border-white z-20"
+            id="home"
+            className="grid grid-cols-1 lg:grid-cols-12 border border-white z-20"
         >
-            <div className="col-span-7 flex flex-col items-center md:items-start justify-center">
+            {/* hero intro & buttons */}
+            <div className="col-span-7 flex flex-col items-center lg:items-start justify-center">
                 <h1 className="text-5xl font-extrabold mb-3">
-                    Hi, I'm Varun
+                    <span className="text-transparent bg-clip-text bg-gradient-to-r from-emerald-400 via-sky-400 to-blue-600">Hi, I'm</span> Varun
                 </h1>
+
+                {/* animated subtitles */}
+                <div className="my-5">
+                    <div>
+                        <p className='m-0 leading-[0px] text-2xl text-center'>
+                            FULL STACK ENGINEER
+                        </p> 
+                        <p className='m-0 leading-[0px] text-2xl text-center'>
+                            WEB DEVELOPER
+                        </p> 
+                        <p className='m-0 leading-[0px] text-2xl text-center'>
+                            PROBLEM SOLVER
+                        </p> 
+                        <p className='m-0 leading-[0px] text-2xl text-center text-amber-400'>
+                            PIZZA LOVER
+                        </p>                   
+                    </div>
+                </div>
                 
-                <p className="text-slate-400 text-lg text-center md:text-left md:text-xl">
+                <span className="text-slate-400 text-lg text-center lg:text-left md:text-xl">
                     Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam maiores nostrum cupiditate quibusdam saepe tenetur deleniti autem molestias aspernatur nam.
-                </p>
+                </span>
+
+                {/* buttons */}
+                <div>
+                    <button className="mx-2 my-5 py-2 px-5 rounded-full bg-gradient-to-r from-blue-400 via-fuchsia-400 to-violet-500 hover:scale-110 transition duration-300">
+                        Hire Me
+                    </button>
+                    <button className="mx-2 my-5 py-2 px-5 rounded-full bg-gradient-to-r from-blue-400 via-fuchsia-400 to-violet-500 hover:scale-110 transition duration-300">
+                        Download Resume
+                    </button>
+                </div>
             </div>
 
+            {/* hero image */}
             <div className="col-span-5 flex justify-center my-5 md:mt-0">
                 <Image 
                     src="/fly-koala.jpg"
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,10 @@ import Hero from './components/Hero'
 
 export default function Home() {
     return (
-        <main className="flex min-h-screen flex-col items-center justify-center bg-black text-white mx-auto">
-            <Hero />
+        <main className="flex min-h-screen flex-col items-center bg-black text-white mx-auto">
+            <div className='container'>
+                <Hero />
+            </div>
         </main>
     )
 }
